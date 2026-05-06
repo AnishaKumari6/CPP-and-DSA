191214ce31908ebfc5651358af59742372fd5c86
AnishaKumari6 <anisha10021kumari@gmail.com>
2026-05-06 23:28:32 +0530

    4th  Day

--- vs code/Bubble_Selection_Insersion.cpp
+++ vs code/Bubble_Selection_Insersion.cpp
@@ -1,24 +1,44 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int main(){
-    int arr[6]={3,1,5,6,2,4};
+int main()
+{
+    int arr[6] = {3, 1, 5, 6, 2, 4};
+    // for (int i = 1; i < 6; i++)
+    // {   
+    //     // Bubble sort
+    //     bool swap = false;
+    //     for (int j = 0; j < 6-1; j++)
+    //     {
+    //         if (arr[i] < arr[j])
+    //         {
+    //             swap = true;
+    //             int temp = arr[j];
+    //             arr[j] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //     }
+    //     if (!swap)
+    //     {
+    //         break;
+    //     }
+    // }
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout << arr[i] << "  ";
+    // }
+
+    // selection sort
     for(int i=0;i<6;i++){
-        bool swap=false;
-        for(int j=i+1;j<6-i-1;j++){
-            if(arr[i]>arr[j]){
-                swap=true;
-            int temp=arr[i];
-            arr[i]=arr[j+1];
-            arr[j+1]=temp;
+        int small=i;
+        for(int j=i+1;j<6;j++){
+            if(arr[j]<=arr[small]){
+                small=j;
             }
         }
-        if(!swap){
-            break;
-        }
+        swap(arr[i],arr[small]);
     }
     for(int i=0;i<6;i++){
-        cout<<arr[i]<<"  ";
+        cout<<arr[i]<<" ";
     }
-    
 }
